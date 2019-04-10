e1a7352f0490804487449ea1e71a2b3770687281
mistertandon <enggparveshtandon@gmail.com>
2019-04-10 13:20:31 +0530

    feat: Part II: Weight Initialization for FashionMNIST dataset
    Develop imshow function definition to display batch of images from
    FashioMNIST dataset.

---
+++ weight-initialization/helpers_self.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def imshow(plt_axis, image, title, normalized=True):
+
+    # Image Un-normalized process
+    image = image.numpy().transpose((1, 2, 0))
+
+    if normalized:
+        mean = np.array([0.5, 0.5, 0.5])
+        std = np.array([0.5, 0.5, 0.5])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+    plt_axis.imshow(image)
+    plt_axis.set_title(title, fontdict={'fontsize': 30})
+    plt_axis.spines['top'].set_visible(False)
+    plt_axis.spines['left'].set_visible(False)
+    plt_axis.spines['bottom'].set_visible(False)
+    plt_axis.spines['right'].set_visible(False)
+    plt_axis.tick_params(axis='both', length=0)
+    plt_axis.set_xticklabels('')
+    plt_axis.set_yticklabels('')
+
+    return plt_axis
